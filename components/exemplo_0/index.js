cc95b207e2ef14493f00124b700c3310595ebfe2
niel02k <138844237+niel02k@users.noreply.github.com>
2024-02-27 15:24:24 -0300

    aula 27-02

---
+++ components/exemplo_0/index.js
@@ -0,0 +1,16 @@
+ import {View, Text} from 'react-native';
+
+ function Exemplo_0 (){
+  
+    return (  
+        <View>
+         <Text> MInha primeira tela</Text>
+
+        </View>
+       );
+
+
+
+ }
+
+     export default Exemplo_0 ;
